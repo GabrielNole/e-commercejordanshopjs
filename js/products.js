613034834c6ec10d10a85411b541c30ff57b47ed
Gabriel Nole <nolegabriel@hotmail.com>
2022-11-19 19:08:07 -0300

    primer commit

---
+++ js/products.js
@@ -0,0 +1,82 @@
+const productos = [
+    {
+      id: 1,
+      nombre: "Air Jordan 1 Retro High OG",
+      precio: 225,
+      img:
+        "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/4e7695a1-3a51-4c22-98e3-3bad6274cee1/air-jordan-1-retro-high-og-shoes.png",
+      cantidad: 1,
+    },
+    {
+      id: 2,
+      nombre: "Air Jordan 1 Low FlyEase",
+      precio: 170,
+      img:
+        "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/c448b55f-60fd-46e9-a818-0d9bc392e8b1/air-jordan-1-low-flyease-easy-on-off-shoes-28LT69.png",
+      cantidad: 1,
+    },
+    {
+      id: 3,
+      nombre: "Air Jordan 1 Mid",
+      precio: 165,
+      img:
+        "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/2d5a9da4-00ab-40dc-a428-7fa5df88b031/air-jordan-1-mid-shoes-WpqHJn.png",
+      cantidad: 1,
+    },
+    {
+      id: 4,
+      nombre: "Air Jordan 1 Low G",
+      precio: 180,
+      img:
+        "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/0a1c535a-5d25-46cb-b439-9c2451c9e8e0/air-jordan-1-low-g-golf-shoes-knwwrG.png",
+      cantidad: 1,
+    },
+    {
+      id: 5,
+      nombre: "Jordan Jumpman Pro",
+      precio: 185,
+      img:
+        "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/f6ed1490-9e36-4994-b32d-098653245611/jordan-jumpman-pro-shoes-xf10xd.png",
+      cantidad: 1,
+    },
+    {
+      id: 6,
+      nombre: "Air Jordan 11 CMFT Low",
+      precio: 170,
+      img:
+        "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/1111f1cb-16a9-47ed-af98-3626c4ed1152/air-jordan-11-cmft-low-shoes-2C7fJQ.png",
+      cantidad: 1,
+    },
+    {
+      id: 7,
+      nombre: "Air Jordan XXXVI 'Taco Jay'",
+      precio: 250,
+      img:
+        "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/7c28c6a9-8720-4b0d-8d1d-930d2e73d519/air-jordan-xxxvi-taco-jay-basketball-shoes-nrm7QG.png",
+      cantidad: 1,
+    },
+    {
+      id: 8,
+      nombre: "Air Jordan 1 Mid",
+      precio: 165,
+      img:
+        "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/b0dcd29c-9fe8-44fd-9b4d-a821cea0cacb/air-jordan-1-mid-shoes-f8W9ns.png",
+      cantidad: 1,
+    },
+    {
+      id: 9,
+      nombre: "Air Jordan 7 Retro Q",
+      precio: 290,
+      img:
+        "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/19fff81f-d2bd-466e-8541-dd1f096961f1/air-jordan-7-retro-q-shoes-v2fW92.png",
+      cantidad: 1,
+    },
+    {
+      id: 10,
+      nombre: "Air Jordan 11 Varsity Red",
+      precio: 290,
+      img:
+        "https://static.nike.com/a/images/c_limit,w_592,f_auto/t_product_v1/06cc2fd7-08e2-4df2-a88b-9862ca923510/air-jordan-11-retro-shoes.png",
+      cantidad: 1,
+    },
+  ];
